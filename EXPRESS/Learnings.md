a0da0c6fbd1bf0c6e407bd5c7e4218cc7779b91d
TALHA ABID <talha.abid@bracketsltd.email>
2024-06-24 11:43:17 +0500

    Handle STATIC Routes Using html, css & js....

--- EXPRESS/Learnings.md
+++ EXPRESS/Learnings.md
@@ -6,3 +6,6 @@ Make Code Manageable With Design Partan Like `MVC` => Export Files & Functions
 MVC_(Model(Consider As DB) , View(Refers To HTMl), Controllers(Includes Operations/Functions Performed On DB According to API))
 Router => (Consider as itself complete app with It's Own Routs i.e all Routs Starts With '/friends')
 Path Api & Image Sharing
+Use Path Api To host Static Files from Public Folder Like HTML or Web Page (Run Server => localhost:3000/home)
+Use Template Engines for Dynamic Change in you View folder  (Run Server => localhost:3000/ )
+Layouts,Formate & Best Practices of using Template Engines (hbs=>> HandleBars)
--- EXPRESS/server.js
+++ EXPRESS/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const path = require("path");
 
 const homeRouter = require("./routers/Home.router");
-const friendRouter = require("./routers/Friends.router")
+const friendRouter = require("./routers/Friends.router");
 const randomRouter = require("./routers/Random.router");
 
 const app = express();
@@ -15,12 +16,21 @@ app.use((req, res, next) => {
   console.log(`API Took : ${Date.now() - dateStart} ms`);
 });
 
+
+// = Home Route to Check Server is running but A better Way Is Above
+app.use("/", homeRouter);
+
+// == A better Way to Check Server is running With a static site
+app.use("/home", express.static(path.join(__dirname, "public", "Static")));
+
+// === A BEST Way to Check Server is running With a Dynamic site rendering
+
+
 // A Default Middleware From EXPRESS use to Convert Buffers To Json Formate
 app.use(express.json());
 
-app.use("/", homeRouter)
-app.use("/friends", friendRouter)
-app.use("/random", randomRouter)
+app.use("/friends", friendRouter);
+app.use("/random", randomRouter);
 
 app.listen(port, () => {
   console.log(`Listening On Port ${port}.....`);
