72cd86e0b3b933096d10e63af97b9645facc3b80
TALHA ABID <talha.abid@bracketsltd.email>
2024-06-24 14:05:20 +0500

    Using template engine (hbs) to build dynamic views, followed by layouts

--- EXPRESS/Learnings.md
+++ EXPRESS/Learnings.md
@@ -8,4 +8,5 @@ Router => (Consider as itself complete app with It's Own Routs i.e all Routs Sta
 Path Api & Image Sharing
 Use Path Api To host Static Files from Public Folder Like HTML or Web Page (Run Server => localhost:3000/home)
 Use Template Engines for Dynamic Change in you View folder  (Run Server => localhost:3000/ )
+Use of Template Engines in Controllers
 Layouts,Formate & Best Practices of using Template Engines (hbs=>> HandleBars)
--- EXPRESS/server.js
+++ EXPRESS/server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const path = require("path");
 
-const homeRouter = require("./routers/Home.router");
+// const homeRouter = require("./routers/Home.router");
 const friendRouter = require("./routers/Friends.router");
 const randomRouter = require("./routers/Random.router");
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
+// ===================================================================================================
+//Template engines in Node development frameworks(Express) do not always need to be explicitly required in code.
+//Instead, they can be loaded or defined, and the views directory path can be set to ensure they are correctly used for rendering views.
+// ===================================================================================================
+// Set the view engine to hbs (handleBars)
+app.set("view engine", "hbs");
+// Set the directory where the template files are located
+app.set("views", path.join(__dirname, "views"));
+
+// ===================================================================================================
 // A Middleware use to track API completion durations
 app.use((req, res, next) => {
   const dateStart = Date.now();
@@ -16,15 +26,19 @@ app.use((req, res, next) => {
   console.log(`API Took : ${Date.now() - dateStart} ms`);
 });
 
-
 // = Home Route to Check Server is running but A better Way Is Above
-app.use("/", homeRouter);
+// app.use("/", homeRouter); // === === ===NOT Recommended
 
-// == A better Way to Check Server is running With a static site
+// ==> A better Way to Check Server is running With a static site ('/home' to differentiate btw better(static) & Best(dynamic) )
 app.use("/home", express.static(path.join(__dirname, "public", "Static")));
 
-// === A BEST Way to Check Server is running With a Dynamic site rendering
-
+// ===> A BEST Way to Check Server is running With a Dynamic site rendering
+app.get('/' ,(req, res)=>{
+  res.render('index',{
+    title:'Node Dynamic Web',
+    caption:'Using Template engine hbs'
+  })
+})
 
 // A Default Middleware From EXPRESS use to Convert Buffers To Json Formate
 app.use(express.json());
@@ -32,6 +46,7 @@ app.use(express.json());
 app.use("/friends", friendRouter);
 app.use("/random", randomRouter);
 
-app.listen(port, () => {
-  console.log(`Listening On Port ${port}.....`);
+// Express is listening to PORT...
+app.listen(PORT, () => {
+  console.log(`Listening On Port ${PORT}.....`);
 });
