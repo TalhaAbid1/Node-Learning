52cc62aec7ec2cb6f65e941b85f246f83ae6bf7c
TALHA ABID <talha.abid@bracketsltd.email>
2024-07-01 11:51:31 +0500

    part OOf Previous Commit

--- 4 NASA Project/Server/src/routes/planets/planets.controller.js
+++ 4 NASA Project/Server/src/routes/planets/planets.controller.js
@@ -1,6 +1,6 @@
-const {planets} = require('../../models/planets.model')
+const {getAllPlanets} = require('../../models/planets.model')
 function httpGetAllPlanets(req, res) {
-    return res.status(200).json(planets)
+    return res.status(200).json(getAllPlanets())
 }
 
 module.exports = {
